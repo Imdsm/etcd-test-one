9cfa2c50c8965f3bb7e67ff8395f0fcc2bd70da2
Adam K Dean <adam@amberfish.co.uk>
2014-11-20 16:48:31 +0000

    Added test files

---
+++ src/index.js
@@ -0,0 +1,9 @@
+var http = require('http'),
+    etcd = new require('node-etcd')();
+
+http.createServer(function(req, res) {
+    var random = Math.floor((Math.random() * 1000) + 1);
+    etcd.set('test-signal', random);
+    res.writeHead(200, {'Content-Type': 'text/plain' });
+    res.end('Setting signal to random number: ' + random);
+}).listen(80);
